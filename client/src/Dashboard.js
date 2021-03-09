3a9ea8953537ce56ade887e04e9e812795919b9c
Evan Wilson <evan@aurbor.com>
2021-03-09 23:55:19 +0000

    Commit

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,15 +1,23 @@
 import * as React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Title } from "react-admin";
+
 const Dashboard = () => (
-  <Card>
-    <Title title="Welcome To The RSO Console!" />
-    <CardContent>
-      This application is an experiment to see if I can build something
-      meaningful with React/Node.js - I am hoping to make this something that I
-      can use to aid my fantasty football activities on Reality Sports Online.
-    </CardContent>
-  </Card>
+  <Grid container>
+    <Paper>
+      <Card>
+        <Title title="Welcome To The RSO Console!" />
+        <CardContent>
+          This application is an experiment to see if I can build something
+          meaningful with React/Node.js - I am hoping to make this something
+          that I can use to aid my fantasty football activities on Reality
+          Sports Online.
+        </CardContent>
+      </Card>
+    </Paper>
+  </Grid>
 );
 export default Dashboard;
